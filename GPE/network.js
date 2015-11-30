0016a0a0cc99c1755a3c593aa9182af57373b647
Alec <akosik7@gmail.com>
2015-11-29 16:20:01 -0800

    Redefined Hierarchy By Levels instead of Edge Directions

--- GPE/network.js
+++ GPE/network.js
@@ -18,37 +18,37 @@ function draw() {
 
     // create a network
     var container = document.getElementById('mynetwork');
-    var nnodeCount1 = document.getElementById('rnodeCount1').value;
-    var ynodeCount1 = document.getElementById('bnodeCount1').value;
-    var nnodeCount2 = document.getElementById('rnodeCount2').value;
-    var ynodeCount2 = document.getElementById('bnodeCount2').value;
-    var nparentCount = document.getElementById('nparentCount').value;
-    var yparentCount = document.getElementById('yparentCount').value;
+    var wnodeCount1 = document.getElementById('wnodeCount1').value;
+    var bnodeCount1 = document.getElementById('bnodeCount1').value;
+    var wnodeCount2 = document.getElementById('wnodeCount2').value;
+    var bnodeCount2 = document.getElementById('bnodeCount2').value;
+    var wparentCount = document.getElementById('wparentCount').value;
+    var bparentCount = document.getElementById('bparentCount').value;
 
     //Create Ancestors
-    for(var i = 0; i < nnodeCount1; i++) {
-        nodes.push({id: 'an' + i, borderWidth: 3, color: {background: 'white', border:'black'}});
+    for(var i = 0; i < wnodeCount1; i++) {
+        nodes.push({id: 'aw' + i, borderWidth: 3, level: 0, color: {background: 'white', border:'black'}});
     }
 
-    for(var i = 0; i < ynodeCount1; i++) {
-        nodes.push({id: 'ay' + i, color: {background: 'black', border: 'black'}});
+    for(var i = 0; i < bnodeCount1; i++) {
+        nodes.push({id: 'ab' + i, level: 0, color: {background: 'black', border: 'black'}});
     }
 
 
     //Create Children and Edges
-    for(var i = 1; i <= nnodeCount2; i++) {
-        var descendant = 'dn' + i;
-        nodes.push({id: descendant, borderWidth: 3, color: {background: 'white', border: 'black'}});
-        for(var k = 0; k < nparentCount; k++) {
-            var parent =  'an' + ((k + i) % nnodeCount1);
+    for(var i = 1; i <= wnodeCount2; i++) {
+        var descendant = 'dw' + i;
+        nodes.push({id: descendant, borderWidth: 3, level: 1, color: {background: 'white', border: 'black'}});
+        for(var k = 0; k < wparentCount; k++) {
+            var parent =  'aw' + ((k + i) % wnodeCount1);
             edges.push({from: parent, to: descendant, arrows: 'to'});
         }
     }
-    for(var i = 1; i <= ynodeCount2; i++) {
-        var descendant = 'dy' + i;
-        nodes.push({id: descendant, color: {background: 'black', border: 'black'}});
-        for(var k = 0; k < yparentCount; k++) {
-            var parent =  'ay' + ((i + k) % ynodeCount1);
+    for(var i = 1; i <= bnodeCount2; i++) {
+        var descendant = 'db' + i;
+        nodes.push({id: descendant, level: 1, color: {background: 'black', border: 'black'}});
+        for(var k = 0; k < bparentCount; k++) {
+            var parent =  'ab' + ((i + k) % bnodeCount1);
             edges.push({from: parent, to: descendant, arrows: 'to'});
         }
     }
@@ -60,7 +60,6 @@ function draw() {
         layout: {
             hierarchical: {
                 direction: "UD",
-                sortMethod: "directed"
             }
         },
 
@@ -90,8 +89,7 @@ function draw() {
             },
             deleteNode: false,
         },
-        interaction: {selectConnectedEdges: true,
-                      zoomView: false,}
+        interaction: {selectConnectedEdges: true}
     };
     network = new vis.Network(container, data, options);
     calculate(nodes, edges);
@@ -105,16 +103,16 @@ function draw() {
 
 function calculate(nodes, edges) {
 
-    var nnodeCount1 = document.getElementById('rnodeCount1').value;
-    var ynodeCount1 = document.getElementById('bnodeCount1').value;
-    var nnodeCount2 = document.getElementById('rnodeCount2').value;
-    var ynodeCount2 = document.getElementById('bnodeCount2').value;
-    var nparentCount = document.getElementById('nparentCount').value;
-    var yparentCount = document.getElementById('yparentCount').value;
+    var wnodeCount1 = document.getElementById('wnodeCount1').value;
+    var bnodeCount1 = document.getElementById('bnodeCount1').value;
+    var wnodeCount2 = document.getElementById('wnodeCount2').value;
+    var bnodeCount2 = document.getElementById('bnodeCount2').value;
+    var wparentCount = document.getElementById('wparentCount').value;
+    var bparentCount = document.getElementById('bparentCount').value;
 
     //Total Number of Ancestors and Descendants
-    var n_a = Number(nnodeCount1) + Number(ynodeCount1);
-    var n_d = Number(nnodeCount2) + Number(ynodeCount2);
+    var n_a = Number(wnodeCount1) + Number(bnodeCount1);
+    var n_d = Number(wnodeCount2) + Number(bnodeCount2);
 
     var connTotal = edges.length;
 
@@ -123,28 +121,32 @@ function calculate(nodes, edges) {
     ////////////////////////////////////////////////////////////
     var connPerAncestor = connTotal/n_a;
     var Cbar_a = [];
+
+    //This works because white nodes are always added before black nodes
+    //To do: make this calculation work no matter what order the array of nodes is created in
+
     //Connections per ancestor (White)
-    for(var j = 0; j < nnodeCount1; j++) {
+    for(var j = 0; j < wnodeCount1; j++) {
         Cbar_a.push(0);
         for(i in edges) {
-            if(edges[i].from.search('n' + j.toString()) != -1) Cbar_a[j]++;
+            if(edges[i].from.search('w' + j.toString()) != -1) Cbar_a[j]++;
         }
     }
     //Connections per ancestor (Black)
-    for(var j = 0; j < ynodeCount1; j++) {
+    for(var j = 0; j < bnodeCount1; j++) {
         Cbar_a.push(0);
         for(i in edges) {
-            if(edges[i].from.search('y' + j.toString()) != -1) Cbar_a[Number(nnodeCount1) + j]++;
+            if(edges[i].from.search('b' + j.toString()) != -1) Cbar_a[Number(wnodeCount1) + j]++;
         }
     }
     //Ratio of Connections per ancestor for a specific ancestor to Overall Connections per ancestor
     Cbar_a = Cbar_a.map(function(x) {return x / connPerAncestor; });
 
     //Character (White or black / 0 or 1) of each ancestor
-        var X_a =[];
+    var X_a =[];
     var j = 0;
     for(i in nodes) {
-        if(nodes[i].id.search('a') != -1) {
+        if(nodes[i].level == 0) {
             if(nodes[i].color.background == 'black') X_a[j] = 1;
             else X_a[j] = 0;
             j++;
@@ -158,7 +160,7 @@ function calculate(nodes, edges) {
 
 
     //Average character of ancestors
-    var meanX_a = Number(ynodeCount1) / X_a.length;
+    var meanX_a = Number(bnodeCount1) / X_a.length;
 
     //Covariance of Cbar_a and X_a
     sum = 0;
@@ -170,17 +172,17 @@ function calculate(nodes, edges) {
     //////////////////////////////////////////////////////////////
 
     var Cbar_d = [];
-    for(var j = 1; j <= Number(nnodeCount2); j++) {
+    for(var j = 1; j <= Number(wnodeCount2); j++) {
         Cbar_d.push(0);
         for(i in edges) {
-            if(edges[i].to.search('n' + j.toString()) != -1) Cbar_d[j-1]++;
+            if(edges[i].to.search('w' + j.toString()) != -1) Cbar_d[j-1]++;
         }
     }
 
-    for(var j = 1; j <= Number(ynodeCount2); j++) {
+    for(var j = 1; j <= Number(bnodeCount2); j++) {
         Cbar_d.push(0);
         for(i in edges) {
-            if(edges[i].to.search('y' + j.toString()) != -1) Cbar_d[Number(nnodeCount2) + j-1]++;
+            if(edges[i].to.search('b' + j.toString()) != -1) Cbar_d[Number(wnodeCount2) + j-1]++;
         }
     }
     var connPerDescendant = connTotal/n_d;
@@ -190,7 +192,7 @@ function calculate(nodes, edges) {
     var X_d =[];
     var j = 0;
     for(i in nodes) {
-        if(nodes[i].id.search('d') != -1) {
+        if(nodes[i].level == 1) {
             if(nodes[i].color.background == 'black') X_d[j] = 1;
             else X_d[j] = 0;
             j++;
@@ -204,7 +206,7 @@ function calculate(nodes, edges) {
 
 
     //Average character of ancestors
-    var meanX_d = Number(ynodeCount2) / X_d.length;
+    var meanX_d = Number(bnodeCount2) / X_d.length;
 
     //Covariance of Cbar_a and X_a
     sum = 0;
@@ -214,8 +216,8 @@ function calculate(nodes, edges) {
     /////////////////////////////////////////////////////////////////
     //Delta Xbar
     /////////////////////////////////////////////////////////////////
-    var Xbar_a = ynodeCount1 / n_a;
-    var Xbar_d = ynodeCount2 / n_d;
+    var Xbar_a = bnodeCount1 / n_a;
+    var Xbar_d = bnodeCount2 / n_d;
     var deltaXbar = Xbar_d-Xbar_a;
 
 
